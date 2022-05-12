4d7fa653391f383b24695f2807090edb1eeaf0a4
MaeveFitzpatrick <mfitz004@gmail.com>
2022-05-12 14:50:31 -0700

    indent adjustments

--- src/myAdvGame/MyAdvGame.java
+++ src/myAdvGame/MyAdvGame.java
@@ -2,26 +2,30 @@ package myAdvGame;
 import java.util.Scanner;
 public class MyAdvGame {
 
-	public static void main {String[] args) {
+	public static void main (String[] args) {
+		
 		Scanner input = new Scanner(System.in);
 		TextBasedGame myGame = new TextBasedGame();
 		myGame.intro();
 		String response = input.next();
 		if (response.equals("mailbox")) {
-		      System.out.println("You opened the mailbox, and found a key inside!\nUse it to open the door (type 'door').\n(or type 'restart' to go back to the beginning)");
-		      myGame.key("key");
-		    }
+			System.out.println("You opened the mailbox, and found a key inside!\nUse it to open the door (type 'door').\n(or type 'restart' to go back to the beginning)");
+			myGame.key("key");
+		}
 		else if (response.equals("door")) {
-		      System.out.println("Uh oh, the door is locked! Type 'quit' to try again.");
-		      myGame.quit("quitResponse");
-		    }
+			System.out.println("Uh oh, the door is locked! Type 'quit' to try again.");
+			myGame.quit("quitResponse");
+		}
 	}
 }
+
 class TextBasedGame {
+	
 	Scanner input = new Scanner(System.in);
 	public void intro() {
 		System.out.println("Welcome to your adventure!\nYou are standing in front of a blue house, next to a mailbox.\nWhat would you like to do?\nOpen the door (type 'door')\nor\nOpen the mailbox (type 'mailbox')");
-	  }
+	}
+	
 	public void open(String openResponse) {
 	    openResponse = input.next();
 	    if (openResponse.equals("mailbox")) {
@@ -30,7 +34,8 @@ class TextBasedGame {
 	    else if (openResponse.equals("door")) {
 	      System.out.println("Uh oh, the door is locked! Type 'restart' to try again.");
 	    }
-	  }
+	}
+	
 	public void key(String keyResponse) {
 	    keyResponse = input.next();
 	    if (keyResponse.equals("door")) {
@@ -40,6 +45,7 @@ class TextBasedGame {
 	      System.out.println("Thanks for playing!");
 	    }
 	}
+	
 	public void quit(String quitResponse) {
 	    quitResponse = input.next();
 	    if (quitResponse.equals("quit")) {
