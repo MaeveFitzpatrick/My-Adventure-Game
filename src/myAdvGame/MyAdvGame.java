b49b24ec9a07b0d50fe9962df6f49de559f298cc
MaeveFitzpatrick <mfitz004@gmail.com>
2022-05-12 14:42:57 -0700

    created intro method

--- src/myAdvGame/MyAdvGame.java
+++ src/myAdvGame/MyAdvGame.java
@@ -4,8 +4,14 @@ public class MyAdvGame {
 
 	public static void main {String[] args) {
 		Scanner input = new Scanner(System.in);
+		TextBasedGame myGame = new TextBasedGame();
+		
 	}
 }
 class TextBasedGame {
 	Scanner input = new Scanner(System.in);
+	public void intro() {
+		System.out.println("Welcome to your adventure!\nYou are standing in front of a blue house, next to a mailbox.\nWhat would you like to do?\nOpen the door (type 'door')\nor\nOpen the mailbox (type 'mailbox')");
+	  }
+	}
 }
