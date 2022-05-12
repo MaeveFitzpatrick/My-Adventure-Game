53d4db53436a8fb1b9289314d5730b5d1b8e6df1
MaeveFitzpatrick <mfitz004@gmail.com>
2022-05-12 14:41:41 -0700

    added class

--- src/myAdvGame/MyAdvGame.java
+++ src/myAdvGame/MyAdvGame.java
@@ -2,8 +2,10 @@ package myAdvGame;
 import java.util.Scanner;
 public class MyAdvGame {
 
-	public MyAdvGame() {
-		
+	public static void main {String[] args) {
+		Scanner input = new Scanner(System.in);
 	}
-
+}
+class TextBasedGame {
+	Scanner input = new Scanner(System.in);
 }
